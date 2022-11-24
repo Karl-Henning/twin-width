44cc4e15691339763823afe08e5df65cb4a3351c
@ccc3001 <johann.strunck@tuhh.de>
2022-11-24 12:09:15 +0100

    fixed some minor mistakes

--- solo-brut.cpp
+++ solo-brut.cpp
@@ -7,10 +7,12 @@ unsigned int vertices [verticesnr];
 
 unsigned int min(unsigned int a, unsigned int b)
 {
-    if(a<=b){
+    if(a <= b)
+    {
         return a; 
     }
-    else{
+    else
+    {
         return b;
     }
 }
@@ -26,12 +28,12 @@ unsigned int max(unsigned int a, unsigned int b)
 }
 
 
-unsigned int mergeVertice(int a, int b, int edges[x][x], unsigned int matrixsize)
+unsigned int mergeVertice(int a, int b, int edges[][], unsigned int verticesnr)
 {
 
-    // a and b should range from 0 to matrix size
+    // a and b should range from 0 to matrix size -1
     //TODO: function that reduces the size of the matrix by 1 
-        for(int i = 0; i < matrixsize; i++)
+        for(unsigned int i = 0; i < verticesnr; i++)
     {
         if(i < a)
         {
@@ -114,7 +116,9 @@ unsigned int mergeVertice(int a, int b, int edges[x][x], unsigned int matrixsize
             }
         }
     }
+
     int newedges[matrixsize-1][matrixsize-1]=0;
+
     for(int c = 0; c < matrixsize; c++ )
     {
         for(int d = c; d < matrixsize; d++ )
@@ -151,13 +155,13 @@ unsigned int mergeVertice(int a, int b, int edges[x][x], unsigned int matrixsize
 }
 
 
-unsigned int rededges(int edges [a][a])
+unsigned int rededges(int edges [][], int verticesnr)
 {
     unsigned int red = 0;
     unsigned int redmax = 0;
-    for(int i = 0; i <= a; i++)
+    for(int i = 0; i <= verticesnr; i++)
     {
-        for(int j = 0; j <=a; j++)
+        for(int j = 0; j <= verticesnr; j++)
         {
             if(edges [i][j] == -1)
             {
@@ -175,7 +179,7 @@ unsigned int rededges(int edges [a][a])
     return redmax;
 }
 
-unsigned int mergeVertices(unsigned int iteration, int vertice unsigned int verticesnr, unsigned int maxiter  )//TODO: check how to optimize the the number of vriables passed 
+unsigned int mergeVertices(int edges[][], unsigned int iteration, int vertice, unsigned int verticesnr, unsigned int maxiter  )//TODO: check how to optimize the the number of vriables passed 
 // iteration must be initialized with 1
 {
     unsigned int maxred = inf;
@@ -198,7 +202,7 @@ unsigned int mergeVertices(unsigned int iteration, int vertice unsigned int vert
             {
                 // reduce matrix and calculate the max twin width by recursion
                 int temp [verticesnr][verticesnr] = mergeVertice(a,b,edges);
-                mincolorvec[i] =max(rededges(temp), mergeVertices(iteration+1, temp, verticesnr-1); //TODO: welche matrix muss übergeben werden 
+                mincolorvec[i] =max(rededges(temp), mergeVertices(iteration+1, temp, verticesnr-1)); //TODO: welche matrix muss übergeben werden 
                 i++;
             }
    }
